c53cdffc976ecc7f093b35689d2003d2bcc2d51e
DEX3350 <panwardeep02@gmail.com>
2023-07-04 14:21:11 +0900

    04-47 auth

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use App\Services\TweetService;
 
 class IndexController extends Controller
 {
     //
     public function a()
     {
-        $tweets=Tweet::all()->sortByDesc('created_at');
-        //dd($tweets);
+        // $tweets=Tweet::all()->sortByDesc('created_at');
+        $tweets= Tweet::orderBy('created_at','DESC')->get();
+        $tweetService = new TweetService();
+        $tweets=$tweetService->getTweets();
         return view('tweet.index')
             ->with('tweets',$tweets);
            
